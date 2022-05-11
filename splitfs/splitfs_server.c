0c69774d4bf4cd7f2a2ff8b9a075d8ca44ef04de
NSaamaja <98792784+NSaamaja@users.noreply.github.com>
2022-05-11 00:15:40 -0500

    added changes

--- splitfs/splitfs_server.c
+++ splitfs/splitfs_server.c
@@ -403,6 +403,7 @@ int handle_pwrite(struct ll_node* node, struct remote_request request) {
 
 	// allocate space to receive the incoming data
 	data_buf = malloc(request.count);
+	printf("server pwrite - trying to see data %s \n", data_buf );
 	if (data_buf == NULL) {
 		perror("malloc");
 		return -1;
