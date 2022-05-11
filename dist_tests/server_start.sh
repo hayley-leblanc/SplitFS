8d59453ebf098f4e3c49c76a1af138f1fa7cb382
rishabkhincha <khincharishab@gmail.com>
2022-05-11 06:35:51 +0000

    sudo for server start

--- dist_tests/server_start.sh
+++ dist_tests/server_start.sh
@@ -1,1 +1,1 @@
-export LEDGER_YCSB=1; cd ../splitfs;  make clean; make; cd ../; export LD_LIBRARY_PATH=./splitfs; export NVP_TREE_FILE=./splitfs/bin/nvp_nvp.tree; cd dist_tests; ./setup.sh; ./run.sh ./server;
+export LEDGER_YCSB=1; cd ../splitfs;  make clean; make; cd ../; export LD_LIBRARY_PATH=./splitfs; export NVP_TREE_FILE=./splitfs/bin/nvp_nvp.tree; cd dist_tests; ./setup.sh; sudo ./run.sh ./server;
